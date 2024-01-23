d13a74c9a276fa672cae1b2f48b815c2ddc72e48
Vimal Raveendran <vimalraveendra@gmail.com>
2024-01-23 13:57:32 +0100

    added odd component, updated app  component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string="Print content"
+  evenNumbers:number[]=[];
+  oddNumbers:number[]=[];
+
+  onIntervalFired(firedNumber:number){
+      if(firedNumber%2===1){
+        this.oddNumbers.push(firedNumber)
+      }else{
+        this.evenNumbers.push(firedNumber)
+      }
+  }
 }
